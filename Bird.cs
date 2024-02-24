4275996a263be08307eefb08b1226cbc2f2f8105
skeadee <127588358+skeadee@users.noreply.github.com>
2024-02-24 21:18:25 +0900

    Update Bird.cs

--- Bird.cs
+++ Bird.cs
@@ -16,6 +16,8 @@ public class Bird : MonoBehaviour
     FlappyGameManager flappy; // 게임을 관리하는 매니저
 
 
+ㅁㄴㅇㅁㄴㅇ
+
     private void Awake()
     {
         flappy = GameObject.Find("FlappyGameManager").GetComponent<FlappyGameManager>();
